7773f92c84f7eb75ca5642a2cb4c8861026e9a68
Alfred Ssekagiri <assekagiri@gmail.com>
2018-05-08 09:09:39 +0300

    Update denovo.sh

--- denovo.sh
+++ denovo.sh
@@ -1,7 +1,7 @@
 #This script performs de novo assembly of any number of samples using dipspades
 #It assumes that reads paired end and are quality trimmed using trim_galore,so feel free to change accordingly if this does not apply to you
-indir='/home/student9/muiiMNGS2/trimmed' #path to reads to be assembled
-outdir='/home/student9/muiiMNGS2/assembly' #path we to store the assembly results
+indir='$HOME/MNGS2/trimmed' #path to reads to be assembled
+outdir='$HOME/MNGS2/assembly' #path we to store the assembly results
 suffix='_R1_val_1.fq' #naming extensions, _left.fastq, _right.fq, blah,blah,blah ...
 fmt='fq'
 
